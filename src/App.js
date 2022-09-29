d8bd63fa1a2de279e09dd7a7dec97c11101a7802
AJHosuja <t1vian03@students.oamk.fi>
2022-09-29 13:52:35 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsss</code> and save to reload.
+          Edit <code>src/App.jsss</code> test.
         </p>
         <a
           className="App-link"
